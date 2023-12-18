66fefc1832ac473d21d4291241cf81f68eb2ef3c
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-18 10:59:07 -0800

    Make make_txtar.sh safer by randomizing directory creating/deleted

--- {{ cookiecutter.project_slug }}/make_txtar.sh
+++ {{ cookiecutter.project_slug }}/make_txtar.sh
@@ -1,7 +1,19 @@
-rm -rf /tmp/{{ cookiecutter.project_slug }}
+tmp=$(mktemp -d /tmp/{{ cookiecutter.project_slug }}.XXXXX)
 
-rm -f /tmp/{{ cookiecutter.project_slug }}.tar
-rm -f /tmp/filelist.txt
+set -e
+
+if [ -z "${tmp+x}" ]; then
+    echo "Error: \$tmp is not set."
+    exit 1
+fi
+
+if [ -z "$tmp" ]; then
+    echo "Error: \$tmp is set but is an empty string."
+    exit 1
+fi
+
+rm -f $tmp/{{ cookiecutter.project_slug }}.tar
+rm -f $tmp/filelist.txt
 
 {
     rg --files . \
@@ -17,9 +29,9 @@ rm -f /tmp/filelist.txt
         # | grep -v cmd/main.go \
         # | grep -v {{ cookiecutter.project_slug }}.go \
 
-} | tee /tmp/filelist.txt
-tar -cf /tmp/{{ cookiecutter.project_slug }}.tar -T /tmp/filelist.txt
-mkdir -p /tmp/{{ cookiecutter.project_slug }}
-tar xf /tmp/{{ cookiecutter.project_slug }}.tar -C /tmp/{{ cookiecutter.project_slug }}
-rg --files /tmp/{{ cookiecutter.project_slug }}
-txtar-c /tmp/{{ cookiecutter.project_slug }} | pbcopy
+} | tee $tmp/filelist.txt
+tar -cf $tmp/{{ cookiecutter.project_slug }}.tar -T $tmp/filelist.txt
+mkdir -p $tmp/{{ cookiecutter.project_slug }}
+tar xf $tmp/{{ cookiecutter.project_slug }}.tar -C $tmp/{{ cookiecutter.project_slug }}
+rg --files $tmp/{{ cookiecutter.project_slug }}
+txtar-c $tmp/{{ cookiecutter.project_slug }} | pbcopy
