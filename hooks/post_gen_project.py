c0a2ed0c9c8787ba3880c1898bd7dc29f41edaea
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-18 12:38:14 -0800

    refactor: allow littlecow external package to bump version

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,24 @@
 import pathlib
 import stat
+import subprocess
+import sys
+
+
+def run_go_mod_tidy() -> bool:
+    try:
+        subprocess.run(["go", "mod", "tidy"], capture_output=True, check=True)
+        return True
+    except Exception:
+        return False
 
 
 def set_script_permissions():
-    for path in pathlib.Path(".").glob('**/*.sh'):
+    for path in pathlib.Path(".").glob("**/*.sh"):
         path.chmod(path.stat().st_mode | stat.S_IEXEC)
 
 
 if __name__ == "__main__":
     set_script_permissions()
+    if not run_go_mod_tidy():
+        print("ERROR: go mod tidy failed.")
+        sys.exit(1)
--- {{ cookiecutter.project_slug }}/go.mod
+++ {{ cookiecutter.project_slug }}/go.mod
@@ -1,5 +1,3 @@
 module github.com/taylormonacelli/{{ cookiecutter.project_slug }}
 
 go 1.21.1
-
-require github.com/taylormonacelli/littlecow v0.0.5
