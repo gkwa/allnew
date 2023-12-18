ef8e2ffb5bbf6f73c28962e65f258340831eaec1
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-18 13:23:57 -0800

    feat: add pre-commit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,6 +12,27 @@ def run_go_mod_tidy() -> bool:
         return False
 
 
+def install_git_pre_commit() -> None:
+    cmd = ["pre-commit", "sample-config"]
+    proc = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    out, err = proc.communicate()
+    return_code = proc.poll()
+    out = out.decode(sys.stdin.encoding)
+    err = err.decode(sys.stdin.encoding)
+
+    config = pathlib.Path(".pre-commit-config.yaml")
+    config.write_text(out)
+
+    if err:
+        msg = f"pre-commit sample-config >{config} failed"
+        raise ValueError(msg)
+
+
 def set_script_permissions() -> None:
     for path in pathlib.Path(".").glob("**/*.sh"):
         path.chmod(path.stat().st_mode | stat.S_IEXEC)
@@ -22,3 +43,4 @@ if __name__ == "__main__":
     if not run_go_mod_tidy():
         print("ERROR: go mod tidy failed.")
         sys.exit(1)
+    install_git_pre_commit()
