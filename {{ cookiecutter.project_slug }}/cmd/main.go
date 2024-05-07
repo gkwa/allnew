161e36000eb56edd0ad5db27c86c2dcc89d16616
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-07 16:30:52 -0700

    support version for allnew

--- {{ cookiecutter.project_slug }}/cmd/main.go
+++ {{ cookiecutter.project_slug }}/cmd/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}"
+	"github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}/version"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		buildInfo := version.GetBuildInfo()
+		fmt.Println(buildInfo)
+		os.Exit(0)
+	}
+
+	versionFlag := flag.Bool("version", false, "Print version information")
+	flag.BoolVar(versionFlag, "v", false, "Print version information")
+
+	flag.Parse()
+
+	if *versionFlag {
+		buildInfo := version.GetBuildInfo()
+		fmt.Println(buildInfo)
+		os.Exit(0)
+	}
+
 	code := {{ cookiecutter.project_slug }}.Execute()
 	os.Exit(code)
 }
